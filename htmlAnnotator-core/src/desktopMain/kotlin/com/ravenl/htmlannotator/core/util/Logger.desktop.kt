83e5b852ab9d5ab928948ef34ae932dd07b3134d
Denis Vysokikh <d.vysokikh@finyard.com>
2025-05-09 12:00:15 +0200

    fix: implement dumb desktop defaultLogger

--- htmlAnnotator-core/src/desktopMain/kotlin/com/ravenl/htmlannotator/core/util/Logger.desktop.kt
+++ htmlAnnotator-core/src/desktopMain/kotlin/com/ravenl/htmlannotator/core/util/Logger.desktop.kt
@@ -1,5 +1,28 @@
 package com.ravenl.htmlannotator.core.util
 
 actual fun defaultLogger(level: Logger.Level): Logger {
-    TODO("Not yet implemented")
+    return Logger(proxy = object : Logger.Proxy {
+        override fun v(tag: String, msg: String, tr: Throwable?) {
+            println("[v]: $tag - $msg ${tr?.message} ")
+        }
+
+        override fun d(tag: String, msg: String, tr: Throwable?) {
+            println("[d]: $tag - $msg ${tr?.message} ")
+        }
+
+        override fun i(tag: String, msg: String, tr: Throwable?) {
+            println("[i]: $tag - $msg ${tr?.message} ")
+        }
+
+        override fun w(tag: String, msg: String, tr: Throwable?) {
+            println("[w]: $tag - $msg ${tr?.message} ")
+        }
+
+        override fun e(tag: String, msg: String, tr: Throwable?) {
+            println("[e]: $tag - $msg ${tr?.message} ")
+        }
+
+        override fun flush() {
+        }
+    })
 }
